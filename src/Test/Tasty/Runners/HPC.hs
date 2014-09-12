c731925230bfd679c1a80d3e4cb91c2c8df81998
Greg Hale <imalsogreg@gmail.com>
2014-09-12 00:01:50 -0400

    Make a monoid to hold hpc results

--- src/Test/Tasty/Runners/HPC.hs
+++ src/Test/Tasty/Runners/HPC.hs
@@ -1,27 +1,50 @@
--- | Run a 'Tasty.TestTree' and produce a summary of which tests cover which
---   parts of the code
+{-# LANGUAGE RankNTypes #-}
 
 module Test.Tasty.Runners.HPC where
 
 ------------------------------------------------------------------------------
+import           Control.Concurrent.MVar
+import qualified Data.Map                as Map
+import           Data.Monoid
 import           Data.Proxy (Proxy(..))
 import           Data.Typeable
 ------------------------------------------------------------------------------
-import qualified Test.Tasty.Options   as Tasty
-import qualified Test.Tasty.Providers as Tasty
-import qualified Test.Tasty.Runners   as Tasty
+import qualified Test.Tasty.Options      as Tasty
+import qualified Test.Tasty.Providers    as Tasty
+import qualified Test.Tasty.Runners      as Tasty
+import qualified Trace.Hpc.Tix           as Hpc
+import qualified Trace.Hpc.Mix           as Hpc
 
 import Test.Tasty.Runners.HPC.Internal
 
 hpcRunner :: Tasty.Ingredient
-hpcRunner = Tasty.TestReporter optionDescription runner
- where
-   optionDescription = [Tasty.Option (Proxy :: Proxy MixPath)
-                       ,Tasty.Option (Proxy :: Proxy TixPath)
-                       ]
+hpcRunner = Tasty.TestManager optionDescriptions runner
+  where
+   optionDescriptions = [Tasty.Option (Proxy :: Proxy MixPath)
+                        ,Tasty.Option (Proxy :: Proxy TixPath)
+                        ]
 
+   runner :: Tasty.OptionSet -> Tasty.TestTree -> Maybe (IO Bool)
    runner options testTree = do
-     MixPath mixPath <- Tasty.lookupOption options
-     TixPath tixPath <- Tasty.lookupOption options
-     undefined
-         
+     let RunHPC  runHpc  = Tasty.lookupOption options
+         MixPath mixPath = Tasty.lookupOption options
+         TixPath tixPath = Tasty.lookupOption options
+     case runHpc of
+       False -> Just (return False)
+       True  -> Just $ do
+         talkingStick <- newMVar ()
+         let hpcFold = Tasty.trivialFold {
+                 Tasty.foldSingle   = runSingle
+               , Tasty.foldResource = (resourceFold talkingStick)
+               }
+         let b = Tasty.foldTestTree hpcFold options testTree :: CodeTests
+         return (not $ b == mempty)
+
+   resourceFold :: forall a. MVar () -> Tasty.ResourceSpec a -> (IO a -> CodeTests) -> CodeTests
+   resourceFold mv (Tasty.ResourceSpec acc rel) rFun = undefined
+
+   hpcRSpec :: MVar () -> Tasty.ResourceSpec ()
+   hpcRSpec m = Tasty.ResourceSpec (takeMVar m) (const $ putMVar m ())
+
+   runSingle :: forall t. Tasty.IsTest t => Tasty.OptionSet -> Tasty.TestName -> t -> CodeTests
+   runSingle = undefined
--- src/Test/Tasty/Runners/HPC/Internal.hs
+++ src/Test/Tasty/Runners/HPC/Internal.hs
@@ -3,13 +3,19 @@
 module Test.Tasty.Runners.HPC.Internal where
 
 import Control.Applicative
+import qualified Data.Map as Map
+import Data.Monoid
 import Data.Proxy  (Proxy(..))
 import Data.Tagged (Tagged(..))
 import Data.Typeable
 
+import qualified Trace.Hpc.Mix        as Mix
+import qualified Test.Tasty           as Tasty
+import qualified Test.Tasty.Providers as Tasty
+import qualified Test.Tasty.Options   as Tasty
 
-import qualified Test.Tasty.Options as Tasty
 
+------------------------------------------------------------------------------
 newtype MixPath = MixPath FilePath
                 deriving (Typeable)
 
@@ -19,11 +25,33 @@ instance Tasty.IsOption MixPath where
   optionName   = Tagged "mixpath"
   optionHelp   = Tagged "Base path for hpc's mix files"
 
+
+------------------------------------------------------------------------------
+newtype TixPath = TixPath FilePath
+                deriving (Typeable)
+
 instance Tasty.IsOption TixPath where
   defaultValue = TixPath "testtuite.tix"
   parseValue   = Just . TixPath
   optionName   = Tagged "tixpath"
   optionHelp   = Tagged "Location of hpc's tix file. $MYTESTSUITE.tix"
 
-newtype TixPath = TixPath FilePath
-                deriving (Typeable)
+
+------------------------------------------------------------------------------
+newtype RunHPC = RunHPC Bool
+               deriving (Typeable)
+
+instance Tasty.IsOption RunHPC where
+  defaultValue = RunHPC False
+  parseValue s = RunHPC <$> Tasty.safeRead s
+  optionName   = Tagged "hpc"
+  optionHelp   = Tagged "Run hpc after each test, map libray code to test code"
+
+
+newtype CodeTests = CodeTests (Map.Map Mix.MixEntry [(Tasty.TestName,Bool)])
+                  deriving (Eq)
+
+------------------------------------------------------------------------------
+instance Monoid CodeTests where
+  mempty                            = CodeTests $ Map.empty
+  CodeTests a `mappend` CodeTests b = CodeTests $ Map.unionWith (++) a b

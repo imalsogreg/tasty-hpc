38fd7ac32eb90d5f13b55f871b56ed3e297040ec
Greg Hale <imalsogreg@gmail.com>
2014-09-16 09:27:23 -0400

    more skeleton ideas about rendering a module

--- src/Test/Tasty/Runners/HPC/Render.hs
+++ src/Test/Tasty/Runners/HPC/Render.hs
@@ -1,5 +1,6 @@
-module Test.Tasty.Runners.HPC.Render where
+{-# LANGUAGE OverloadedStrings #-}
 
+module Test.Tasty.Runners.HPC.Render where
 
 ------------------------------------------------------------------------------
 import qualified Language.Haskell.Exts.Annotated as HSE
@@ -8,12 +9,19 @@ import qualified Text.Blaze.Html                 as Html
 import qualified Trace.Hpc.Mix                   as Hpc
 import qualified Trace.Hpc.Tix                   as Hpc
 import qualified Trace.Hpc.Util                  as Hpc
+import qualified Text.Blaze                      as B
+import           Text.Blaze
+import qualified Text.Blaze.Html5                as B
+import           Text.Blaze.Html5.Attributes
 ------------------------------------------------------------------------------
 import           Test.Tasty.Runners.HPC.Internal 
 
+renderModule :: (HSE.Module HSE.SrcSpanInfo) -> Html.Html
+renderModule = undefined
 
 srcDeclToHtml :: HSE.Decl HSE.SrcLoc -> ModuleTests -> Html.Html
-srcDeclToHtml src (ModuleTests testMap) =
+srcDeclToHtml decl (ModuleTests testMap) =
   let tests = [] in  -- TODO find the tests for this scr node
-  
+  case decl of
+    (HSE.TypeDecl _ _ _) -> B.div ! href "#TypeDecl" $ B.toHtml (HSE.prettyPrint decl)
   
